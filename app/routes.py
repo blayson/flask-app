92682ca2a336a63fd9d281e857e35be1867e0b04
Andrew <blayson4@gmail.com>
2018-03-27 16:45:41 +0200

    Add Ajax translation for posts

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 
-from flask import flash, redirect, render_template, request, url_for, g
+from flask import flash, redirect, render_template, request, url_for, g, jsonify
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
+from guess_language import guess_language
 
 from app import app, db
 from app.email import send_password_reset_email
 from app.forms import (EditProfileForm, LoginForm, PostForm, RegistrationForm,
                        ResetPasswordRequestForm, ResetPasswordForm)
 from app.models import Post, User
+from app.translate import translate
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -18,7 +20,11 @@ from app.models import Post, User
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(
+            body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -183,7 +189,8 @@ def reset_password_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
-        flash(_('Check your email for the instructions to reset your password'))
+        flash(
+            _('Check your email for the instructions to reset your password'))
         return redirect(url_for('login'))
     return render_template(
         'reset_password_request.html', title=_('Reset Password'), form=form)
@@ -203,3 +210,13 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({
+        'text': translate(request.form['text'],
+                          request.form['source_language'],
+                          request.form['dest_language'])
+    })
