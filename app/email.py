b10c32e49660ad5b9a91929da1257f3c4119ce51
Andrew <blayson4@gmail.com>
2018-03-27 13:26:41 +0200

    Add translations (flask-babel) and cli

--- app/email.py
+++ app/email.py
@@ -1,6 +1,8 @@
 from threading import Thread
 
 from flask_mail import Message
+from flask_babel import _
+
 from app import mail
 
 
@@ -14,7 +16,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
-        '[Microblog] Reset Your Password',
+        _('[Microblog] Reset Your Password'),
         sender=app.config['ADMINS'][0],
         recipients=[user.email],
         text_body=render_template(
