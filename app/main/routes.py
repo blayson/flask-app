dfbecd43ae2bc9c327b31cd3f30891b5c3b0a94a
Andrew <blayson4@gmail.com>
2018-06-01 16:14:52 +0200

    Add messages and notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,8 +9,10 @@ from guess_language import guess_language
 from app import db
 from app.main import bp
 from app.main.forms import EditProfileForm, PostForm, SearchForm
-from app.models import Post, User
+from app.models import Post, User, Notification
 from app.translate import translate
+from app.main.forms import MessageForm
+from app.models import Message
 
 
 @bp.before_app_request
@@ -31,8 +33,7 @@ def index():
         language = guess_language(form.post.data)
         if language == 'UNKNOWN' or len(language) > 5:
             language = ''
-        post = Post(
-            body=form.post.data, author=current_user, language=language)
+        post = Post(author=current_user, body=form.post.data, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -105,8 +106,8 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template(
-        'edit_profile.html', title=_('Edit Profile'), form=form)
+    return render_template('edit_profile.html', title=_('Edit Profile'),
+                           form=form)
 
 
 @bp.route('/follow/<username>')
@@ -145,9 +146,8 @@ def unfollow(username):
 @login_required
 def translate_text():
     return jsonify({
-        'text':
-        translate(request.form['text'], request.form['source_language'],
-                  request.form['dest_language'])
+        'text': translate(request.form['text'], request.form['source_language'],
+                          request.form['dest_language'])
     })
 
 
@@ -162,7 +162,8 @@ def search():
     next_url = url_for(
         'main.search',
         q=g.search_form.q.data,
-        page=page +1) if total > page * current_app.config['POSTS_PER_PAGE'] else None
+        page=page + 1) if total > page * current_app.config[
+        'POSTS_PER_PAGE'] else None
     prev_url = url_for(
         'main.search',
         q=g.search_form.q.data,
@@ -180,3 +181,51 @@ def search():
 def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user_popup.html', user=user)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user,
+                      body=form.message.data)
+        user.add_notification('unread_message_count', user.new_messages())
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
